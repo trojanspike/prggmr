a0e3d0f4fd380a31f0a77c01315e918bbb4dc9a9
nwhiting <nwhiting@xstudiosinc.com>
2011-08-17 18:11:35 -0400

    added docs testing file

---
+++ examples/test.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Test file to put all example code in the docs.
+ *
+ * Run this as "php test.php" or in your browser http://yoururl/prggmr/examples/test.php
+ */
+require '../lib/prggmr.php';
+
+// STICK YOUR CODE BELOW HERE
