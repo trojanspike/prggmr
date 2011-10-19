e2f57dd2ea3d04a9c16d2662985ea5fb7b29bde1
Nickolas Whiting <prggmr@gmail.com>
2011-10-18 20:02:21 -0400

    Documentation is now searchable without a database

---
+++ docs/js/plugins.js
@@ -0,0 +1,299 @@
+/*
+ * jQuery Hotkeys Plugin
+ * Copyright 2010, John Resig
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ *
+ * Based upon the plugin by Tzury Bar Yochay:
+ * http://github.com/tzuryby/hotkeys
+ *
+ * Original idea by:
+ * Binny V A, http://www.openjs.com/scripts/events/keyboard_shortcuts/
+*/
+
+(function(jQuery){
+	
+	jQuery.hotkeys = {
+		version: "0.8",
+
+		specialKeys: {
+			8: "backspace", 9: "tab", 13: "return", 16: "shift", 17: "ctrl", 18: "alt", 19: "pause",
+			20: "capslock", 27: "esc", 32: "space", 33: "pageup", 34: "pagedown", 35: "end", 36: "home",
+			37: "left", 38: "up", 39: "right", 40: "down", 45: "insert", 46: "del", 
+			96: "0", 97: "1", 98: "2", 99: "3", 100: "4", 101: "5", 102: "6", 103: "7",
+			104: "8", 105: "9", 106: "*", 107: "+", 109: "-", 110: ".", 111 : "/", 
+			112: "f1", 113: "f2", 114: "f3", 115: "f4", 116: "f5", 117: "f6", 118: "f7", 119: "f8", 
+			120: "f9", 121: "f10", 122: "f11", 123: "f12", 144: "numlock", 145: "scroll", 191: "/", 224: "meta"
+		},
+	
+		shiftNums: {
+			"`": "~", "1": "!", "2": "@", "3": "#", "4": "$", "5": "%", "6": "^", "7": "&", 
+			"8": "*", "9": "(", "0": ")", "-": "_", "=": "+", ";": ": ", "'": "\"", ",": "<", 
+			".": ">",  "/": "?",  "\\": "|"
+		}
+	};
+
+	function keyHandler( handleObj ) {
+		// Only care when a possible input has been specified
+		if ( typeof handleObj.data !== "string" ) {
+			return;
+		}
+		
+		var origHandler = handleObj.handler,
+			keys = handleObj.data.toLowerCase().split(" ");
+	
+		handleObj.handler = function( event ) {
+			// Don't fire in text-accepting inputs that we didn't directly bind to
+			if ( this !== event.target && (/textarea|select/i.test( event.target.nodeName ) ||
+				 event.target.type === "text") ) {
+				return;
+			}
+			
+			// Keypress represents characters, not special keys
+			var special = event.type !== "keypress" && jQuery.hotkeys.specialKeys[ event.which ],
+				character = String.fromCharCode( event.which ).toLowerCase(),
+				key, modif = "", possible = {};
+
+			// check combinations (alt|ctrl|shift+anything)
+			if ( event.altKey && special !== "alt" ) {
+				modif += "alt+";
+			}
+
+			if ( event.ctrlKey && special !== "ctrl" ) {
+				modif += "ctrl+";
+			}
+			
+			// TODO: Need to make sure this works consistently across platforms
+			if ( event.metaKey && !event.ctrlKey && special !== "meta" ) {
+				modif += "meta+";
+			}
+
+			if ( event.shiftKey && special !== "shift" ) {
+				modif += "shift+";
+			}
+
+			if ( special ) {
+				possible[ modif + special ] = true;
+
+			} else {
+				possible[ modif + character ] = true;
+				possible[ modif + jQuery.hotkeys.shiftNums[ character ] ] = true;
+
+				// "$" can be triggered as "Shift+4" or "Shift+$" or just "$"
+				if ( modif === "shift+" ) {
+					possible[ jQuery.hotkeys.shiftNums[ character ] ] = true;
+				}
+			}
+
+			for ( var i = 0, l = keys.length; i < l; i++ ) {
+				if ( possible[ keys[i] ] ) {
+					return origHandler.apply( this, arguments );
+				}
+			}
+		};
+	}
+
+	jQuery.each([ "keydown", "keyup", "keypress" ], function() {
+		jQuery.event.special[ this ] = { add: keyHandler };
+	});
+
+})( jQuery );
+
+function sprintf() {
+  // http://kevin.vanzonneveld.net
+  // +   original by: Ash Searle (http://hexmen.com/blog/)
+  // + namespaced by: Michael White (http://getsprink.com)
+  // +    tweaked by: Jack
+  // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // +      input by: Paulo Freitas
+  // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // +      input by: Brett Zamir (http://brett-zamir.me)
+  // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // *     example 1: sprintf("%01.2f", 123.1);
+  // *     returns 1: 123.10
+  // *     example 2: sprintf("[%10s]", 'monkey');
+  // *     returns 2: '[    monkey]'
+  // *     example 3: sprintf("[%'#10s]", 'monkey');
+  // *     returns 3: '[####monkey]'
+  var regex = /%%|%(\d+\$)?([-+\'#0 ]*)(\*\d+\$|\*|\d+)?(\.(\*\d+\$|\*|\d+))?([scboxXuidfegEG])/g;
+  var a = arguments,
+      i = 0,
+      format = a[i++];
+
+  // pad()
+  var pad = function(str, len, chr, leftJustify) {
+    if (!chr) {
+      chr = ' ';
+    }
+    var padding = (str.length >= len) ? '' : Array(1 + len - str.length >>> 0).join(chr);
+    return leftJustify ? str + padding : padding + str;
+  };
+
+  // justify()
+  var justify = function(value, prefix, leftJustify, minWidth, zeroPad, customPadChar) {
+    var diff = minWidth - value.length;
+    if (diff > 0) {
+      if (leftJustify || !zeroPad) {
+        value = pad(value, minWidth, customPadChar, leftJustify);
+      } else {
+        value = value.slice(0, prefix.length) + pad('', diff, '0', true) + value.slice(prefix.length);
+      }
+    }
+    return value;
+  };
+
+  // formatBaseX()
+  var formatBaseX = function(value, base, prefix, leftJustify, minWidth, precision, zeroPad) {
+    // Note: casts negative numbers to positive ones
+    var number = value >>> 0;
+    prefix = prefix && number && {
+      '2': '0b',
+      '8': '0',
+      '16': '0x'
+    }[base] || '';
+    value = prefix + pad(number.toString(base), precision || 0, '0', false);
+    return justify(value, prefix, leftJustify, minWidth, zeroPad);
+  };
+
+  // formatString()
+  var formatString = function(value, leftJustify, minWidth, precision, zeroPad, customPadChar) {
+    if (precision != null) {
+      value = value.slice(0, precision);
+    }
+    return justify(value, '', leftJustify, minWidth, zeroPad, customPadChar);
+  };
+
+  // doFormat()
+  var doFormat = function(substring, valueIndex, flags, minWidth, _, precision, type) {
+    var number;
+    var prefix;
+    var method;
+    var textTransform;
+    var value;
+
+    if (substring == '%%') {
+      return '%';
+    }
+
+    // parse flags
+    var leftJustify = false,
+        positivePrefix = '',
+        zeroPad = false,
+        prefixBaseX = false,
+        customPadChar = ' ';
+    var flagsl = flags.length;
+    for (var j = 0; flags && j < flagsl; j++) {
+      switch (flags.charAt(j)) {
+      case ' ':
+        positivePrefix = ' ';
+        break;
+      case '+':
+        positivePrefix = '+';
+        break;
+      case '-':
+        leftJustify = true;
+        break;
+      case "'":
+        customPadChar = flags.charAt(j + 1);
+        break;
+      case '0':
+        zeroPad = true;
+        break;
+      case '#':
+        prefixBaseX = true;
+        break;
+      }
+    }
+
+    // parameters may be null, undefined, empty-string or real valued
+    // we want to ignore null, undefined and empty-string values
+    if (!minWidth) {
+      minWidth = 0;
+    } else if (minWidth == '*') {
+      minWidth = +a[i++];
+    } else if (minWidth.charAt(0) == '*') {
+      minWidth = +a[minWidth.slice(1, -1)];
+    } else {
+      minWidth = +minWidth;
+    }
+
+    // Note: undocumented perl feature:
+    if (minWidth < 0) {
+      minWidth = -minWidth;
+      leftJustify = true;
+    }
+
+    if (!isFinite(minWidth)) {
+      throw new Error('sprintf: (minimum-)width must be finite');
+    }
+
+    if (!precision) {
+      precision = 'fFeE'.indexOf(type) > -1 ? 6 : (type == 'd') ? 0 : undefined;
+    } else if (precision == '*') {
+      precision = +a[i++];
+    } else if (precision.charAt(0) == '*') {
+      precision = +a[precision.slice(1, -1)];
+    } else {
+      precision = +precision;
+    }
+
+    // grab value using valueIndex if required?
+    value = valueIndex ? a[valueIndex.slice(0, -1)] : a[i++];
+
+    switch (type) {
+    case 's':
+      return formatString(String(value), leftJustify, minWidth, precision, zeroPad, customPadChar);
+    case 'c':
+      return formatString(String.fromCharCode(+value), leftJustify, minWidth, precision, zeroPad);
+    case 'b':
+      return formatBaseX(value, 2, prefixBaseX, leftJustify, minWidth, precision, zeroPad);
+    case 'o':
+      return formatBaseX(value, 8, prefixBaseX, leftJustify, minWidth, precision, zeroPad);
+    case 'x':
+      return formatBaseX(value, 16, prefixBaseX, leftJustify, minWidth, precision, zeroPad);
+    case 'X':
+      return formatBaseX(value, 16, prefixBaseX, leftJustify, minWidth, precision, zeroPad).toUpperCase();
+    case 'u':
+      return formatBaseX(value, 10, prefixBaseX, leftJustify, minWidth, precision, zeroPad);
+    case 'i':
+    case 'd':
+      number = (+value) | 0;
+      prefix = number < 0 ? '-' : positivePrefix;
+      value = prefix + pad(String(Math.abs(number)), precision, '0', false);
+      return justify(value, prefix, leftJustify, minWidth, zeroPad);
+    case 'e':
+    case 'E':
+    case 'f':
+    case 'F':
+    case 'g':
+    case 'G':
+      number = +value;
+      prefix = number < 0 ? '-' : positivePrefix;
+      method = ['toExponential', 'toFixed', 'toPrecision']['efg'.indexOf(type.toLowerCase())];
+      textTransform = ['toString', 'toUpperCase']['eEfFgG'.indexOf(type) % 2];
+      value = prefix + Math.abs(number)[method](precision);
+      return justify(value, prefix, leftJustify, minWidth, zeroPad)[textTransform]();
+    default:
+      return substring;
+    }
+  };
+
+  return format.replace(regex, doFormat);
+}
+
+function getUrlVars()
+{
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
+/**
+ * http://www.JSON.org/json2.js 2011-02-23 Public Domain.
+ */
+var JSON;if(!JSON){JSON={};}(function(){"use strict";function f(n){return n<10?'0'+n:n;}if(typeof Date.prototype.toJSON!=='function'){Date.prototype.toJSON=function(key){return isFinite(this.valueOf())?this.getUTCFullYear()+'-'+f(this.getUTCMonth()+1)+'-'+f(this.getUTCDate())+'T'+f(this.getUTCHours())+':'+f(this.getUTCMinutes())+':'+f(this.getUTCSeconds())+'Z':null;};String.prototype.toJSON=Number.prototype.toJSON=Boolean.prototype.toJSON=function(key){return this.valueOf();};}var cx=/[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,escapable=/[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,gap,indent,meta={'\b':'\\b','\t':'\\t','\n':'\\n','\f':'\\f','\r':'\\r','"':'\\"','\\':'\\\\'},rep;function quote(string){escapable.lastIndex=0;return escapable.test(string)?'"'+string.replace(escapable,function(a){var c=meta[a];return typeof c==='string'?c:'\\u'+('0000'+a.charCodeAt(0).toString(16)).slice(-4);})+'"':'"'+string+'"';}function str(key,holder){var i,k,v,length,mind=gap,partial,value=holder[key];if(value&&typeof value==='object'&&typeof value.toJSON==='function'){value=value.toJSON(key);}if(typeof rep==='function'){value=rep.call(holder,key,value);}switch(typeof value){case'string':return quote(value);case'number':return isFinite(value)?String(value):'null';case'boolean':case'null':return String(value);case'object':if(!value){return'null';}gap+=indent;partial=[];if(Object.prototype.toString.apply(value)==='[object Array]'){length=value.length;for(i=0;i<length;i+=1){partial[i]=str(i,value)||'null';}v=partial.length===0?'[]':gap?'[\n'+gap+partial.join(',\n'+gap)+'\n'+mind+']':'['+partial.join(',')+']';gap=mind;return v;}if(rep&&typeof rep==='object'){length=rep.length;for(i=0;i<length;i+=1){if(typeof rep[i]==='string'){k=rep[i];v=str(k,value);if(v){partial.push(quote(k)+(gap?': ':':')+v);}}}}else{for(k in value){if(Object.prototype.hasOwnProperty.call(value,k)){v=str(k,value);if(v){partial.push(quote(k)+(gap?': ':':')+v);}}}}v=partial.length===0?'{}':gap?'{\n'+gap+partial.join(',\n'+gap)+'\n'+mind+'}':'{'+partial.join(',')+'}';gap=mind;return v;}}if(typeof JSON.stringify!=='function'){JSON.stringify=function(value,replacer,space){var i;gap='';indent='';if(typeof space==='number'){for(i=0;i<space;i+=1){indent+=' ';}}else if(typeof space==='string'){indent=space;}rep=replacer;if(replacer&&typeof replacer!=='function'&&(typeof replacer!=='object'||typeof replacer.length!=='number')){throw new Error('JSON.stringify');}return str('',{'':value});};}if(typeof JSON.parse!=='function'){JSON.parse=function(text,reviver){var j;function walk(holder,key){var k,v,value=holder[key];if(value&&typeof value==='object'){for(k in value){if(Object.prototype.hasOwnProperty.call(value,k)){v=walk(value,k);if(v!==undefined){value[k]=v;}else{delete value[k];}}}}return reviver.call(holder,key,value);}text=String(text);cx.lastIndex=0;if(cx.test(text)){text=text.replace(cx,function(a){return'\\u'+('0000'+a.charCodeAt(0).toString(16)).slice(-4);});}if(/^[\],:{}\s]*$/.test(text.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g,'@').replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g,']').replace(/(?:^|:|,)(?:\s*\[)+/g,''))){j=eval('('+text+')');return typeof reviver==='function'?walk({'':j},''):j;}throw new SyntaxError('JSON.parse');};}}());
---
+++ docs/js/search.js
@@ -0,0 +1,255 @@
+(function(){
+    'use strict'
+    
+    if (!$.prggmr) {
+        $.prggmr = {}
+    }
+    
+    // prggmr pages to index
+    $.prggmr.pages = [
+        'engine.html',
+        'event.html'
+    ];
+    
+    // the index
+    $.prggmr.indexed = []
+    
+    // simply word shortener
+    function shorten(text) {
+        if (text.length <= 60) return text;
+        var returnText = '';
+        for (var i=0;i!=60;i++) {
+            returnText += text[i];
+        }
+        return returnText+'...';
+    }
+    
+    // the search
+    $.prggmr.initSearch = function(){
+     
+        // index the content
+        for (var i = 2; i != 4; i++) {
+            $('h'+i).each(function(){
+                
+                var heading = $(this).text().replace("\n","").replace("\n","");
+                var text = $(this).next().text().replace("\n","").replace("\n","");
+                var id = $(this).attr('id');
+                
+                $.prggmr.indexed.push({
+                    'heading': heading,
+                    'text': text,
+                    'id': id,
+                    'type': 'h'+i
+                });
+            });
+        }
+        
+        // make the search
+        $('body').append($('<div/>',{
+            'id': 'prggmr-search',
+            'html': '<div class="icon"><img src="js/toolbar_find.png" /></div>'+
+                    '<div class="finder"><div class="input"><input class="search-input" type="text" />'+
+                    '</div><div class="results"></div></div>'
+        }));
+        
+        // search DOM objects
+        var search = $('#prggmr-search');
+        var finder = $('#prggmr-search .finder');
+        var icon = $('#prggmr-search .icon');
+        var input = $('#prggmr-search input');
+        var results = $('#prggmr-search .results');
+        
+        // show the search
+        icon.click(function(){
+            if (finder.is(':hidden')) {
+                icon.addClass('active');
+                finder.fadeIn(150);
+                input.focus();
+                $('div.document').one('click', function(){
+                    icon.click();
+                });
+            } else {
+                icon.removeClass('active');
+                finder.fadeOut(150);
+            }
+        });
+        
+         // search
+        $.prggmr.search = function(value) {
+
+            var top = []
+            var rel = []
+            var regx = new RegExp(value.replace(/^\s\s*/, '').replace(/\s\s*$/, ''));
+            var tmp = {};
+            
+            // two arrays are kept
+            // one for top results 
+            // the other for relevant
+            
+            $.each($.prggmr.indexed, function(_i, _array) { 
+                if (regx.test(_array['heading'].toLowerCase()) !== false && !tmp[_i]) {
+                    tmp[_i] = true;
+                    top.push($.prggmr.indexed[_i]);
+                }
+            });
+
+            $.each($.prggmr.indexed, function(_i, _array) { 
+                if (regx.test(_array['text'].toLowerCase()) !== false && !tmp[_i]) {
+                    tmp[_i] = true;
+                    rel.push($.prggmr.indexed[_i]);
+                }
+            });
+
+            if (top.length === 0) {
+                top.push({
+                    'id': 'null',
+                    'heading': 'No results found',
+                    'text': null
+                });
+            } 
+            
+            if (rel.length === 0) {
+                rel.push({
+                    'id': 'null',
+                    'heading': 'No results found',
+                    'text': null
+                });
+            }
+            
+            return {
+                'rel' : rel,
+                'top' : top
+            }
+        }
+            
+        input.keyup(function(){
+
+            var result = $.prggmr.search($(this).val())
+            var top = result['top'];
+            var rel = result['rel'];
+
+            results.html('');
+            results.append($('<ul/>', {
+                'id': 'result-list'
+            }));
+            var result_list = $('#result-list');
+            
+            result_list.append($('<li/>', {
+                'class': 'heading',
+                'html': 'Top Results'
+            }));
+            
+            $.each(top, function(_i, _item){
+                result_list.append($('<li/>',{
+                    'class': 'result',
+                    'html': sprintf(
+                        '<a href="%s">%s</a><div class="copy">%s</div>',
+                        '#'+_item.id,
+                        shorten(_item.heading),
+                        _item.text
+                    )
+                }));
+            });
+            
+            result_list.append($('<li/>', {
+                'class': 'heading',
+                'html': 'Relevant'
+            }));
+            
+            $.each(rel, function(_i, _item){
+                result_list.append($('<li/>',{
+                    'class': 'result',
+                    'html': sprintf(
+                        '<a href="%s">%s</a><div class="copy">%s</div>',
+                        '#'+_item.id,
+                        shorten(_item.heading),
+                        _item.text
+                    )
+                }));
+            });
+        });
+        
+        /**
+         * Passes through next and prev search result items.
+         
+         * true = next, false = prev
+         */
+        $.prggmr.keyResults = function(dir) {
+            if (finder.is(':hidden')) return true;
+            var found = false;
+            $('ul li.result', results).each(function(){
+                if ($(this).hasClass('active')) {
+                    found = true;
+                    var item = (dir) ? $(this).next() : $(this).prev();
+                    // couldn't find a fast way ... todo: Find a better method
+                    if (item.hasClass('heading')) item = (dir) ? item.next() : item.prev();
+                    if (!found) {
+                        // reverse
+                        if (item.length === 0) {
+                            var item = (dir) ? $('ul li.result:first', results) : $('ul li.result:last', results);
+                        }
+                        // how?
+                        if (item.length === 0) return false;
+                        found = true;
+                        $(this).trigger('mouseleave');
+                        item.trigger('mouseenter');
+                        item.focus();
+                    }
+                }
+                return true;
+            });
+            
+            if (!found) {
+                $('ul li.result:first', results).trigger('mouseenter');
+            }
+            return false;
+        }
+        
+        
+        // Key combinations
+        
+        // key comb 'alt+space' shows search
+        $(document).bind('keydown', 'alt+space', function(){
+            icon.click();
+            return false;
+        });
+        
+        // alt+down/alt+up moves through results
+        $(document, 'input').bind('keydown', 'down', function(){
+            return $.prggmr.keyResults(true);
+        });
+        
+        // alt+down/alt+up moves through results
+        $(document, 'input').bind('keydown', 'up', function(){
+            return $.prggmr.keyResults(false);
+        });
+        
+        // alt+enter goes to current result
+        $(document).bind('keydown', 'alt+enter', function(){
+            console.log('year');
+            $('ul li', results).each(function(){
+                if ($(this).hasClass('active')) {
+                    $(this).click();
+                }
+            });
+            return false;
+        });
+        
+        $('ul li', results).live('mouseenter', function(){
+            $(this).addClass('active');
+            $('div.copy', this).fadeIn(100);
+        }).live('mouseleave', function(){
+            $(this).removeClass('active');
+            $('div.copy', this).fadeOut(100);
+        });
+        
+        // SEARCH USING ?search=SEARCH
+        var PARAMS = getUrlVars();
+        
+        if (PARAMS['search']) {
+            console.log($.prggmr.search(PARAMS['search']));
+            $('html').html(JSON.stringify($.prggmr.search(PARAMS['search'])));
+        }
+    }
+    
+})(jQuery);
