983a5230aecf508c7bc4f5d673d892d91233401b
Nickolas Whiting <prggmr@gmail.com>
2012-03-23 14:54:03 -0400

    Added non-blocking server example

---
+++ tests/server.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Non-Blocking Server Example
+ */
+handle(function(){
+    $this->headers(['Content-Type': 'text/plain']);
+    $this->write('Hello World');
+    return 200;
+}, \prggmr\network\Server('127.0.0.1', 1337));
+
+print "Server is running at 127.0.0.1:1337";
