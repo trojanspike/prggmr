eedf6c2c9947aebd4091fcdb03044300bd49d6fe
Nickolas Whiting <prggmr@gmail.com>
2012-03-26 09:10:34 -0400

    EngineException provides a message using the engine_code util, added tests for engine exceptions.

--- tests/engine_performance.php
+++ tests/engine_performance.php
@@ -8,13 +8,15 @@ $event->handle = 0;
 echo memory_get_usage().PHP_EOL;
 $time = microtime(true);
 for($i=0;$i!=100;$i++) {
-    for ($a = 0;$a!=1;$a++) {
+    for ($a = 0;$a!=10;$a++) {
         $handle_count++;
         handle(function(){
             $this->handle++;
-        }, $i);
+        }, $i, 0, null);
     }
-    signal($i, null, $event);
+    // signal($i, null, $event);
 }
+echo "Setup $handle_count handles".PHP_EOL;
+echo "Handled ".$event->handle." times".PHP_EOL;
 echo microtime(true) - $time;
 echo PHP_EOL.memory_get_peak_usage().PHP_EOL;
---
+++ tests/exceptions.php
@@ -0,0 +1,21 @@
+<?php
+
+require '../src/prggmr.php';
+
+// INVALID_HANDLE
+//handle('a', 'b');
+
+// HANDLE_EXCEPTION
+// handle(function(){
+//     throw new Exception('test');
+// }, 'error');
+// signal('error');
+
+// INVALID_SIGNAL
+//signal(array());
+
+// INVALID_EVENT
+//signal(array(), null, $str = 'asd');
+
+// INVALID_HANDLE_DIRECTORY
+//handle_loader('a', '/asjhfasf');
