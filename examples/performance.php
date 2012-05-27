cdbf7d7b266203c4311c91152a9a954197aa86e3
Nickolas Whiting <prggmr@gmail.com>
2012-05-27 09:53:20 -0400

    Undoing the changes made to the engine for the ops test, seems PHP 5.4.3 has provided some of a performance increase.

--- examples/performance.php
+++ examples/performance.php
@@ -1,14 +1,14 @@
 <?php
 ini_set('memory_limit', -1);
 $time = microtime(true);
-for ($i=0;$i!=100;$i++){
+for ($i=0;$i!=10000;$i++){
     prggmr\handle(function() use ($i){}, $i);
 }
 echo "Handle Register".PHP_EOL;
 echo microtime(true) - $time;
 echo PHP_EOL;
 $time = microtime(true);
-for ($i=0;$i!=100;$i++){
+for ($i=0;$i!=10000;$i++){
     prggmr\signal($i);
 }
 echo "Signal Calls".PHP_EOL;
