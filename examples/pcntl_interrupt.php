52295a1b7932439d43868d3505638e5ca7e6fc49
♠ prggmr <prggmr@gmail.com>
2012-05-31 14:47:16 -0400

    Removed the declare ticks from prggmr ... wow did it slow it down

--- examples/pcntl_interrupt.php
+++ examples/pcntl_interrupt.php
@@ -1,11 +1,17 @@
 <?php
-
+// you must declare ticks for this signal
+declare(ticks=1);
 prggmr\load_signal('pcntl');
-
 prggmr\handle(function(){
-    echo "CATCHING SHUTDOWN";
+    echo PHP_EOL."CATCHING SHUTDOWN".PHP_EOL;
+    // if you dont exit it will not exit on SIGINT
     exit;
 }, new prggmr\signal\pcntl\Interrupt());
 
+// $engine = new prggmr\Engine();
+// $engine->handle(function(){
+//     echo PHP_EOL."CATCHING SHUTDOWN".PHP_EOL;
+//     exit
+// }, new prggmr\signal\pcntl\Interrupt($engine));
 // inifite loop to demonstrate ctrl-c to exit
 while(1){}
